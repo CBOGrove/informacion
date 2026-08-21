a43eeebf373e0f3f8d4f5ce1f480db3f0fc5380c
CB O Grove <cbogrove75@gmail.com>
2026-08-21 12:42:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // ===== CONFIGURACIÓN GLOBAL =====
-// Versión 2.2 - Rutas para https://cbogrove.github.io/informacion/
+// Versión 2.3 - Añadida configuración de Xornadas
 
 const CONFIG = {
     // Google Sheets
@@ -8,14 +8,23 @@ const CONFIG = {
         plantillas: 'https://docs.google.com/spreadsheets/d/1HyII2lYStJ8AdgL1vjWPc7N9S_4r4eCHKH-4WMLHD24/export?format=csv',
         patrocinadores: 'https://docs.google.com/spreadsheets/d/1vTw0TksIAImMJy4zUnmkgum_FTpLdFe_0UDghEOPbiA/export?format=csv',
         sorteos: 'https://docs.google.com/spreadsheets/d/1we-1DRNJjbrut1lzJZFwBwxSWCcGrRZ1D4Afb4QYIyY/export?format=csv',
+        // NUEVO: Xornadas
+        xornadas: 'https://docs.google.com/spreadsheets/d/1RBZgpB7SU-zYtfs_yFlxeFbA76sfwj4v0ceQpmto3Fk/export?format=csv',
     },
     
-    // Google Drive - imágenes (rutas relativas desde la raíz del sitio)
+    // Google Sheets - gid para cada equipo en Xornadas
+    xornadasGid: {
+        femenino: '0',
+        masculino: '1780874499'
+    },
+    
+    // Google Drive - imágenes (rutas relativas)
     drive: {
         femenino: 'assets/img/plantillas/femenino/',
         masculino: 'assets/img/plantillas/masculino/',
         logos: 'assets/img/logos/club/',
         patrocinadores: 'assets/img/logos/patrocinadores/',
+        rivales: 'assets/img/logos/rivales/',
     },
     
     // Twitch
