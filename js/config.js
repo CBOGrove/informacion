c8bb5bf7820654251d731ec3282e89a695edc734
CB O Grove <cbogrove75@gmail.com>
2026-08-21 11:26:06 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
 // ===== CONFIGURACIÓN GLOBAL =====
+// Versión 2.2 - Rutas para https://cbogrove.github.io/informacion/
+
 const CONFIG = {
     // Google Sheets
     sheets: {
@@ -6,15 +8,14 @@ const CONFIG = {
         plantillas: 'https://docs.google.com/spreadsheets/d/1HyII2lYStJ8AdgL1vjWPc7N9S_4r4eCHKH-4WMLHD24/export?format=csv',
         patrocinadores: 'https://docs.google.com/spreadsheets/d/1vTw0TksIAImMJy4zUnmkgum_FTpLdFe_0UDghEOPbiA/export?format=csv',
         sorteos: 'https://docs.google.com/spreadsheets/d/1we-1DRNJjbrut1lzJZFwBwxSWCcGrRZ1D4Afb4QYIyY/export?format=csv',
-        // Jornadas: pendiente
     },
     
-    // Google Drive - imágenes
+    // Google Drive - imágenes (rutas relativas desde la raíz del sitio)
     drive: {
-        femenino: '/assets/img/plantillas/femenino/',
-        masculino: '/assets/img/plantillas/masculino/',
-        logos: '/assets/img/logos/club/',
-        patrocinadores: '/assets/img/logos/patrocinadores/',
+        femenino: 'assets/img/plantillas/femenino/',
+        masculino: 'assets/img/plantillas/masculino/',
+        logos: 'assets/img/logos/club/',
+        patrocinadores: 'assets/img/logos/patrocinadores/',
     },
     
     // Twitch
@@ -25,5 +26,4 @@ const CONFIG = {
     numJugadores: 5,
 };
 
-// ===== EXPORTAR =====
 window.CONFIG = CONFIG;
